da9919977109f4bee22c92403f2aa99b05476e0d
Ambroise Bareux <abareux@k1r4p6.42mulhouse.fr>
2024-01-20 13:45:42 +0100

    launch init script

--- launch.sh
+++ launch.sh
@@ -2,4 +2,4 @@ docker run --name "kali" -p 3390:3390 --expose=3390 --tty --detach kalilinux/kal
 docker exec "kali" apt-get update
 docker exec "kali" apt-get install -y wget
 docker exec "kali" wget https://github.com/coookiecoder/kali.git
-docker exec "kali" /kali/init.sh
+docker exec "kali" bash /kali/init.sh
