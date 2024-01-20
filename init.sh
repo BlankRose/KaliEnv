8be98165854275f067f3a143532bb206fc67b1e0
Ambroise Bareux <abareux@k1r1p2.42mulhouse.fr>
2024-01-20 17:05:03 +0100

    useradd ==> adduser

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@ apt update && DEBIAN_FRONTEND=noninteractive apt install -y wget kali-linux-head
 apt-get install -y kali-desktop-kde xorg xrdp
 sed -i 's/port=3389/port=3390/g' /etc/xrdp/xrdp.ini
 sudo /etc/init.d/xrdp start
-useradd kali
+adduser kali
 echo 'kali:kali' | sudo chpasswd
