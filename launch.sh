93f93a260f121f40ca19ab9d11e94aae174e3114
Ambroise Bareux <abareux@k1r4p6.42mulhouse.fr>
2024-01-20 13:44:01 +0100

    update

--- launch.sh
+++ launch.sh
@@ -1,2 +1,5 @@
 docker run --name "kali" -p 3390:3390 --expose=3390 --tty --detach kalilinux/kali-rolling
-docker exec "kali" /init.sh
+docker exec "kali" apt-get update
+docker exec "kali" apt-get install -y wget
+docker exec "kali" wget https://github.com/coookiecoder/kali.git
+docker exec "kali" /kali/init.sh
