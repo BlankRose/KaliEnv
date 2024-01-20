bec54ac6676ae560618136596bb019b51e1b8ac6
Ambroise Bareux <abareux@k1r4p6.42mulhouse.fr>
2024-01-20 14:48:56 +0100

    make the container usable trough terminal

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
-docker run --name "kali" -p 3390:3390 --expose=3390 --tty --detach kalilinux/kali-rolling
+docker run --name "kali" -p 3390:3390 --expose=3390 --tty --detach --interactive kalilinux/kali-rolling
 docker exec "kali" apt-get update
 docker exec "kali" apt-get install -y git
 docker exec "kali" git clone https://github.com/coookiecoder/kali.git
