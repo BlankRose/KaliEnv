c1268b04d93250867bc68de27e4652a33eac068e
Ambroise Bareux <abareux@k1r1p2.42mulhouse.fr>
2024-01-23 18:02:23 +0100

    purge take --all to purge image

--- purge.sh
+++ purge.sh
@@ -1,3 +1,10 @@
 docker stop "kali"
+
+if [ $1 = "--all" ]
+then
+	echo "Purging docker && image" && docker rmi "kalilinux/kali-rolling"
+else
+	echo "Purging docker"
+fi
+
 docker rm "kali"
-docker rmi "kalilinux/kali-rolling"
