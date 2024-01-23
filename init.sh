925b9be61ca1bba8bdfcecde77e52de6552fbf30
Ambroise Bareux <abareux@k1r1p2.42mulhouse.fr>
2024-01-23 17:05:54 +0100

    remove kali-linux-headless

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-apt update && DEBIAN_FRONTEND=noninteractive apt install -y wget kali-linux-headless
+apt update && DEBIAN_FRONTEND=noninteractive apt install -y wget
 apt-get install -y kali-desktop-kde xorg xrdp
 sed -i 's/port=3389/port=3390/g' /etc/xrdp/xrdp.ini
 sudo /etc/init.d/xrdp start
